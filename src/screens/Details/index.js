22d06dbfc3eaf9ac0b4f4992abd85a16763e8a13
matheuspenachioni <matheus.penachioni2001@gmail.com>
2022-10-06 16:11:15 -0300

    edit product page

---
+++ src/screens/Details/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react"
+import { 
+    View, 
+    Text, 
+    TextInput, 
+    TouchableOpacity 
+} from "react-native"
+import { FontAwesome } from "@expo/vector-icons"
+import firebase from "../../config/firebaseconfig.js"
+import styles from "./style.js"
+
+export default function Details({ navigation, route }) {
+    const [nomeEdit, setNomeEdit] = useState(route.params.nome)
+    const [marcaEdit, setMarcaEdit] = useState(route.params.marca)
+    const [precoEdit, setPrecoEdit] = useState(route.params.preco)
+    const idProduct = route.params.id
+    const database = firebase.firestore()
+
+    function editProduct(nome, marca, preco, id){
+        //route.params.idUser
+        database.collection("Produtos").doc(id).update({
+            nome: nomeEdit,
+            marca: marcaEdit,
+            preco: precoEdit,
+        })
+        navigation.navigate("Games")
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+                style={styles.inputText}
+                placeholder="Marvel's Spider-Man"
+                placeholderTextColor="#282b2db5"
+                onChangeText={setNomeEdit}
+                value={nomeEdit}
+            />
+            <Text style={styles.label}>Developer</Text>
+            <TextInput
+                style={styles.inputText}
+                placeholder="Insomniac Games"
+                placeholderTextColor="#282b2db5"
+                onChangeText={setMarcaEdit}
+                value={marcaEdit}
+            />
+            <Text style={styles.label}>Price (BRL)</Text>
+            <TextInput
+                style={styles.inputText}
+                placeholder="R$ 249,90"
+                placeholderTextColor="#282b2db5"
+                onChangeText={setPrecoEdit}
+                value={precoEdit}
+            />
+            <TouchableOpacity
+                style={styles.buttonNewProduct}
+                onPress={() => {
+                    editProduct(nomeEdit, marcaEdit, precoEdit, idProduct)
+                }}
+            >
+                <FontAwesome
+                    name="save"
+                    size={23}
+                    color="#fff"
+                >
+                </FontAwesome>
+            </TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/screens/Details/style.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    label: {
+        width: "90%",
+        marginTop: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        fontSize: 16,
+        color: "#325ca8",
+    },
+    inputText: {
+        width: "90%",
+        marginTop: 10,
+        padding: 10,
+        height: 50,
+        borderBottomWidth: 1,
+        borderBottomColor: "#325ca8",
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    buttonNewProduct: {
+        width: 60,
+        height: 60,
+        position: 'absolute',
+        bottom: 30,
+        left: 20,
+        backgroundColor: "#325ca8",
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
+
+export default styles;
