f92cf3b3e92060f060c146022857441fa6131887
matheuspenachioni <matheus.penachioni2001@gmail.com>
2022-10-06 16:00:38 -0300

    firebase config

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCs8sfqDtN9VO7JQ5EPLtzfu-O9mS20zBg",
+  authDomain: "app-projeto-4e430.firebaseapp.com",
+  projectId: "app-projeto-4e430",
+  storageBucket: "app-projeto-4e430.appspot.com",
+  messagingSenderId: "718562037479",
+  appId: "1:718562037479:web:2f7f0c707074ce8b851c42"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
